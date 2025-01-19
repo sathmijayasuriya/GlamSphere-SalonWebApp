e24d4dd1c6f4d715f3b364b7ea85e6579ae2bd3c
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-01-20 00:21:15 +0530

    chnages

--- src/components/footers/FooterAuth.jsx
+++ src/components/footers/FooterAuth.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
-import { Box, Container, Grid, Typography, Link, IconButton } from "@mui/material";
+import React from "react";
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+  Link,
+  IconButton,
+} from "@mui/material";
 import {
   Facebook as FacebookIcon,
   Twitter as TwitterIcon,
@@ -12,73 +19,98 @@ import {
 export default function FooterAuth() {
   return (
     <>
-  <Box
-      component="footer"
-      sx={{
-        backgroundColor: "black",
-        color: "rgba(189, 135, 135, 0.65)",
-        pt: 6,
-        pb: 3,
-      }}
-    >
-      <Container>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Typography
-              variant="h5"
-              sx={{ color: "white", display: "flex", alignItems: "center" }}
-            >
-              <RoomIcon sx={{ mr: 1 }} />
-              Athurugiriya road, Kottawa
-            </Typography>
-            <Link
-              href="mailto:info@company.com"
-              underline="none"
-              sx={{ color: "rgba(255, 255, 255, 0.65)", mt: 2, display: "inline-block" }}
-            >
-              glamsphere@gmail.com
-            </Link>
-          </Grid>
-          <Grid item xs={6}>
-            <Typography
-              variant="body2"
-              sx={{ color: "rgba(255, 255, 255, 0.45)", mb: 0 }}
-            >
-              xxxxx
-              <br />
-              <br />
-              xxxx:{" "}
-              <Link
-                href="https://google.com/"
-                target="_parent"
-                sx={{ color: "inherit" }}
+      <Box
+        component="footer"
+        sx={{
+          backgroundColor: "black",
+          color: "rgba(189, 135, 135, 0.65)",
+          pt: 6,
+          pb: 3,
+        }}
+      >
+        <Container>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Typography
+                variant="h5"
+                sx={{ color: "white", display: "flex", alignItems: "center" }}
               >
-                xxx
+                <RoomIcon sx={{ mr: 1 }} />
+                Athurugiriya road, Kottawa
+              </Typography>
+              <Link
+                href="mailto:info@company.com"
+                underline="none"
+                sx={{
+                  color: "rgba(255, 255, 255, 0.65)",
+                  mt: 2,
+                  display: "inline-block",
+                }}
+              >
+                glamsphere@gmail.com
               </Link>
-            </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "rgba(255, 255, 255, 0.45)",
+                  mb: 0,
+                  "&:hover": { color: "#715332" },
+                }}
+              >
+                Privacy policy
+                <br />
+                <br />
+                <Typography
+                  variant="body2"
+                  sx={{
+                    color: "inherit",
+                    "&:hover": { color: "#715332" },
+                    textTransform: "none",
+                  }}
+                >
+                  © 2025 Glam Sphere. All Rights Reserved
+                </Typography>
+              </Typography>
+            </Grid>
+            <Grid item lg={3} xs={5} sx={{ ml: "auto" }}>
+              <Box sx={{ display: "flex", gap: 1 }}>
+                <IconButton
+                  href="#"
+                  sx={{ color: "white", "&:hover": { color: "#715332" } }}
+                >
+                  <FacebookIcon />
+                </IconButton>
+                <IconButton
+                  href="#"
+                  sx={{ color: "white", "&:hover": { color: "#715332" } }}
+                >
+                  <TwitterIcon />
+                </IconButton>
+                <IconButton
+                  href="#"
+                  sx={{ color: "white", "&:hover": { color: "#715332" } }}
+                >
+                  <WhatsAppIcon />
+                </IconButton>
+                <IconButton
+                  href="#"
+                  sx={{ color: "white", "&:hover": { color: "#715332" } }}
+                >
+                  <InstagramIcon />
+                </IconButton>
+                <IconButton
+                  href="#"
+                  sx={{ color: "white", "&:hover": { color: "#715332" } }}
+                >
+                  <YouTubeIcon />
+                </IconButton>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item lg={3} xs={5} sx={{ ml: "auto" }}>
-            <Box sx={{ display: "flex", gap: 1 }}>
-              <IconButton href="#" sx={{ color: "white" }}>
-                <FacebookIcon />
-              </IconButton>
-              <IconButton href="#" sx={{ color: "white" }}>
-                <TwitterIcon />
-              </IconButton>
-              <IconButton href="#" sx={{ color: "white" }}>
-                <WhatsAppIcon />
-              </IconButton>
-              <IconButton href="#" sx={{ color: "white" }}>
-                <InstagramIcon />
-              </IconButton>
-              <IconButton href="#" sx={{ color: "white" }}>
-                <YouTubeIcon />
-              </IconButton>
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
     </>
-  )
+  );
 }
